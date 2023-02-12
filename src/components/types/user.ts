9fd5c981e19837bdb016bca97685c9bbc3f25e3e
Oleg Astashin <95183292+ophlop@users.noreply.github.com>
2023-02-13 00:03:47 +0400

    -- littel fix in types

--- src/components/types/user.ts
+++ src/components/types/user.ts
@@ -1,37 +1,36 @@
 export type localGithubUser = {
-    login: string,
-    id: number,
-    avatar: string,
-    name: string,
-    company: string,
-    blog: string,
-    location: string,
-    bio: string,
-    twitter: string,
-    repos: number,
-    followers: number,
-    following: number,
-    created: string,
-}
-
+  login: string;
+  id: number;
+  avatar: string;
+  name: string;
+  company: string;
+  blog: string;
+  location: string;
+  bio: string;
+  twitter: string;
+  repos: number;
+  followers: number;
+  following: number;
+  created: string;
+};
 
 export type GithubUser = {
-    login: string,
-    id: number,
-    avatar_url: string,
-    name: string,
-    company: string,
-    blog: string,
-    location: string,
-    bio: string,
-    twitter_username: string,
-    repos: number,
-    followers: number,
-    following: number,
-    created_at: string,
-}
+  login: string;
+  id: number;
+  avatar_url: string;
+  name: string;
+  company: string;
+  blog: string;
+  location: string;
+  bio: string;
+  twitter_username: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+  created_at: string;
+};
 
 export type GithubError = {
-    message: string,
-    documentation_url: string,
-}
+  message: string;
+  documentation_url: string;
+};
