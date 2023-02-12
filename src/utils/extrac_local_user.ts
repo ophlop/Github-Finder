f7c038046cee3034c2c1ae546a00fa213089b6d0
Oleg Astashin <95183292+ophlop@users.noreply.github.com>
2023-02-13 00:04:31 +0400

    -- logic type checker

---
+++ src/utils/extrac_local_user.ts
@@ -0,0 +1,17 @@
+import { GithubUser, localGithubUser } from "../components/types";
+
+export const extractLocalUser = (user: GithubUser): localGithubUser => ({
+  login: user.login,
+  id: user.id,
+  avatar: user.avatar_url,
+  name: user.name,
+  company: user.company,
+  blog: user.blog,
+  location: user.location,
+  bio: user.bio,
+  twitter: user.twitter_username,
+  repos: user.public_repos,
+  followers: user.followers,
+  following: user.following,
+  created: user.created_at,
+});
