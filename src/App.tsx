7c9ccabbaca957625580e172725b305d7e32d9e4
Oleg Astashin <95183292+ophlop@users.noreply.github.com>
2023-02-13 00:03:14 +0400

    -- add logic searching developers

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
-import { Container } from './components/Container/Container';
-import { Header } from './components/Header/Header';
-import { Search } from './components/Search/Search';
-import { UserCard } from './components/UserCard/UserCard';
+import { useState } from "react";
+import { Container } from "./components/Container/Container";
+import { Header } from "./components/Header/Header";
+import { Search } from "./components/Search/Search";
+import { GithubError, GithubUser, localGithubUser } from "./components/types";
+import { UserCard } from "./components/UserCard/UserCard";
+import { defaultUser } from "./mock/index";
+import { extractLocalUser } from "./utils/extrac_local_user";
+import { isGithubUser } from "./utils/type_guards";
 
-import { defaultUser } from './mock/index'
+const urlFetch = "https://api.github.com/users/";
 
 function App() {
+  const [user, setUser] = useState<localGithubUser | null>(defaultUser);
+
+  const fetchUser = async (username: string) => {
+    const url = urlFetch + username;
+
+    const res = await fetch(url);
+    const user = (await res.json()) as GithubUser | GithubError;
+
+    if (isGithubUser(user)) {
+      setUser(extractLocalUser(user));
+    } else {
+      setUser(null);
+    }
+  };
+
   return (
     <div>
-        <Container>
-          <Header />
-          <Search hasError onSubmit={() => {}}/>
-          <UserCard
-            {...defaultUser}
-          />
-        </Container>
+      <Container>
+        <Header />
+        <Search hasError={!user} onSubmit={fetchUser} />
+        {user && <UserCard {...user} />}
+      </Container>
     </div>
   );
 }
