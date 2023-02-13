79d975bb61905e02c3caf41da2ac0d748f5a9efa
Astashin Oleg <astashinoleg@gmail.com>
2023-02-13 11:23:29 +0300

    -- fix component to adaptive

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -25,12 +25,12 @@ const handleSubmit = (event: React.FormEvent<HTMLFormElement & FormFields>) => {
     
     return (
         <form onSubmit={handleSubmit} autoComplete='off'>
-            <div className='relative w-full h-[70px] mt-[30px] overflow-hidden pl-8 pr-8 bg-white flex items-center rounded-2xl md:pr-8'>
+            <div className='relative w-full h-[70px] mt-[30px] overflow-hidden pl-8 pr-8 bg-white flex items-center rounded-2xl md:pr-8 sm:w-full'>
                 <label htmlFor="" className='cursor-pointer'><SearchIcon /></label>
                     <input
                         name='username'
                         type="text"
-                        className='px-3 py-0 grow-[5] max-w-[calc(100% - 120px)] cursor-pointer
+                        className='px-3 py-0 grow-[5] w-[calc(100% - 120px)] cursor-pointer
                                  placeholder:text-gray-200 focus:outline-none
                                    md:mx-1 md:my-0 md:text-xl'
                         id='searchDev'
@@ -38,7 +38,7 @@ const handleSubmit = (event: React.FormEvent<HTMLFormElement & FormFields>) => {
                     />
                     { hasError && (
                         <div className='absolute right-[150px] bottom-1 text-red-400 text-base font-bold
-                                        md:right-{120px} md:top-1/2 md:bottom-unset md:-translate-y-1/2'>
+                                        md:top-1/2 md:bottom-unset md:-translate-y-1/2'>
                             No result
                         </div>
                     )}
