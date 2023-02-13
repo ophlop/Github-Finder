9dee6dc3287294dc0cba0d74a10f2fe1011ca007
Astashin Oleg <astashinoleg@gmail.com>
2023-02-13 11:22:54 +0300

    -- fix component to adaptive

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -5,7 +5,7 @@ interface IContainer {
 }
 
 export const Container = ({children}: IContainer) => (
-    <div className='w-6/12 h-fit py-0 px-6 md:w-734 md:p-0 md:my-0 md:mx-auto'>
+    <div className='lg:w-6/12 h-fit py-0 px-6  md:p-0 md:my-0 md:mx-auto md:w-10/12 sm:w-12'>
         {children}
     </div>
 );
